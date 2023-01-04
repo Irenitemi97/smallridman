df16879e997e9e78e8149915027448a34c8c71b5
idriskelani <idmega2000@gmail.com>
2023-01-04 22:10:12 +0100

    update form code

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -2,7 +2,7 @@
 (function () {
   "use strict";
 
-  let forms = document.querySelectorAll('.php-email-form');
+  let forms = document.querySelectorAll('.php-email-form-use');
 
   forms.forEach( function(e) {
     e.addEventListener('submit', function(event) {
